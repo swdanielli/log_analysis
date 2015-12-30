c9fbd176602a05350b0f96129c274f5731d7d98a
swdanielli <swdanielli@github.com>
2015-12-29 23:22:20 -0500

    back up

--- script/plot_activities.py
+++ script/plot_activities.py
@@ -112,20 +112,14 @@ def get_submission_idx(n_submissions, submission_type):
     return range(1, n_submissions-1)
 
 def compute_accumulated_activities(user_pset_logs, submission_type='all_sub'):
-  #sum_logs = np.zeros((len(groups), len(materials)))
   sum_logs = [[[] for _ in range(len(materials))] for _ in range(len(groups))]
   count_submissions = [[] for _ in range(len(groups))]
-  #count_submissions = np.zeros(len(groups))
-  #count_logs = np.zeros((len(groups), len(materials)))
   active_learners = [[[] for _ in range(len(materials))] for _ in range(len(groups))]
   sum_time_by_user = copy.deepcopy(group_X_material_dict)
 
   for pset in psets:
-    #sum_pset_logs = np.zeros((len(groups), len(materials)))
     sum_pset_logs = [[[] for _ in range(len(materials))] for _ in range(len(groups))]
-    #count_pset_submissions = np.zeros(len(groups))
     count_pset_submissions = [[] for _ in range(len(groups))]
-    #count_pset_logs = np.zeros((len(groups), len(materials)))
     sum_pset_time_by_user = copy.deepcopy(group_X_material_dict)
 
     print_before_stat(pset)
@@ -134,8 +128,6 @@ def compute_accumulated_activities(user_pset_logs, submission_type='all_sub'):
       for username, user_logs in user_pset_logs[group].iteritems():
         if pset in user_logs and is_active(user_logs[pset], submission_type):
           submissions = get_submission_idx(len(user_logs[pset]), submission_type)
-          #count_submissions[group_idx] += len(submissions)
-          #count_pset_submissions[group_idx] += len(submissions)
           count_submissions[group_idx].append(len(submissions))
           count_pset_submissions[group_idx].append(len(submissions))
 
@@ -146,12 +138,6 @@ def compute_accumulated_activities(user_pset_logs, submission_type='all_sub'):
             # only learners active in a certain material should be counted
             # for the usage(time) of that material
             if sum_time:
-              '''
-              sum_logs[group_idx][material_idx] += sum_time
-              sum_pset_logs[group_idx][material_idx] += sum_time
-              count_logs[group_idx][material_idx] += 1
-              count_pset_logs[group_idx][material_idx] += 1
-              '''
               sum_logs[group_idx][material_idx].append(sum_time)
               sum_pset_logs[group_idx][material_idx].append(sum_time)
               active_learners[group_idx][material_idx].append(username)
@@ -164,115 +150,112 @@ def compute_accumulated_activities(user_pset_logs, submission_type='all_sub'):
             sum_pset_time_by_user[condition][username] = sum_time
 
       print_stat(group, sum_pset_logs[group_idx], count_pset_submissions[group_idx])
-      #print_stat(group, sum_pset_logs[group_idx], count_pset_submissions[group_idx], count_pset_logs[group_idx])
     yield sum_pset_time_by_user
 
   print_before_stat('All')
   for group_idx, group in enumerate(groups):
     print_stat(group, sum_logs[group_idx], count_submissions[group_idx], active_learners=active_learners[group_idx])
-    #print_stat(group, sum_logs[group_idx], count_submissions[group_idx], count_logs[group_idx], active_learners=active_learners[group_idx])
 
   yield sum_time_by_user
+
+def gen_stratification(iterable, tags, lambda=):
 
 def _main( ):
   user_listname = sys.argv[1]
   plot_dir = sys.argv[2]
   pset_event_analysis_dir = sys.argv[3]
+  user_cohorts = {}
+  stratification = sys.argv[4]
+  user_profile = sys.argv[5]
+  if 'verified' in stratification:
+    user_cohort_names = ['honor', 'verify']
+    for (header, user) in analytics_util.load_csv_like(user_profile, '\t'):
+      result = analytics_util.parse_common_sql(header, user, ['user_id', 'mode'])
+      user_cohorts[result[0]] = (0 if result[1] == 'honor' else 1)
+  elif 'overall_scores_0.5' in stratification:
+    user_cohort_names = ['lt_0.5', 'gt_0.5']
+    for (header, user) in analytics_util.load_csv_like(user_profile, '\t'):
+      result = analytics_util.parse_common_sql(header, user, ['user_id', 'grade'])
+      user_cohorts[result[0]] = (0 if float(result[1]) == 'honor' else 1)
+  else:
+    user_cohort_names = ['all']
+    for (header, user) in analytics_util.load_csv_like(user_profile, '\t'):
+      result = analytics_util.parse_common_sql(header, user, ['user_id'])
+      user_cohorts[result[0]] = 0
 
-  user_lists = {}
-  user_pset_logs = {}
-  for group in groups:
-    user_lists[group] = analytics_util.load_user_infos('%s/%s' % (group, user_listname))
+  for user_cohort_index, user_cohort_name in enumerate(user_cohort_names):
+    print '\n********** %s learners **********\n' % user_cohort_name
+    if not os.path.exists('%s/%s' % (plot_dir, user_cohort_name)):
+      os.makedirs('%s/%s' % (plot_dir, user_cohort_name))
 
-    user_pset_logs[group] = {}
-    for username in user_lists[group].keys():
-      log_filename = '%s/%s/accumulation-%s' % (group, pset_event_analysis_dir, username)
-      if not os.path.isfile(log_filename):
-        continue
-      user_pset_logs[group][username] = json.load(open(log_filename))
+    user_lists = {}
+    user_pset_logs = {}
+    for group in groups:
+      user_lists[group] = analytics_util.load_user_infos(
+        '%s/%s' % (group, user_listname),
+        lambda x: user_cohorts and (x not in user_cohorts or user_cohorts[x] != user_cohort_index) # stratification
+      )
 
-  for submission_type in ['all_sub', 'first_sub', 'other_sub', 'all', 'first']:
-    print '\n========== %s ==========\n' % sub_type_definition[submission_type]
-    for pset_idx, logs in enumerate(compute_accumulated_activities(user_pset_logs, submission_type)):
-      if not any(logs.values()):
-        continue
+      user_pset_logs[group] = {}
+      for username in user_lists[group].keys():
+        log_filename = '%s/%s/accumulation-%s' % (group, pset_event_analysis_dir, username)
+        if not os.path.isfile(log_filename):
+          continue
+        user_pset_logs[group][username] = json.load(open(log_filename))
 
-      for material in materials:
-        fig = plt.figure()
-        plt.clf()
-        ax = fig.add_subplot(111)
+    for submission_type in ['all_sub', 'first_sub', 'other_sub', 'all', 'first']:
+      print '\n========== %s ==========\n' % sub_type_definition[submission_type]
+      for pset_idx, logs in enumerate(compute_accumulated_activities(user_pset_logs, submission_type)):
+        if not any(logs.values()):
+          continue
 
+        for material in materials:
+          fig = plt.figure()
+          plt.clf()
+          ax = fig.add_subplot(111)
 
-        (time, scores) = ([], [])
-        for group_idx, group in enumerate(groups):
-          for username, sum_time in logs['%s_%s' % (group, material)].iteritems():
-            time.append(sum_time)
-            scores.append(float(user_lists[group][username][0]))
-        #ax.scatter(time, scores, c=colors[group_idx], label=group)
-
-        pylab.xscale('log')
-        pylab.xlim((1, 1000000))
-
-        pylab.title('materials = %s, pset = %s, time span = %s' % (
-          re.sub('_time', '', material),
-          (re.sub('/', '_', psets_pretty[pset_idx]) if pset_idx < len(psets_pretty) else 'all'),
-          sub_type_definition[submission_type]
-        ))
-        pylab.xlabel("spent time (seconds)")
-        pylab.ylabel("scores")
-        errorplot(time, scores, bins=[1,10,100,1000,10000,100000,1000000])
-        '''
-        # plot by groups
-        for group_idx, group in enumerate(groups):
           (time, scores) = ([], [])
-          for username, sum_time in logs['%s_%s' % (group, material)].iteritems():
-            time.append(sum_time)
-            scores.append(float(user_lists[group][username][0]))
-          ax.scatter(time, scores, c=colors[group_idx], label=group)
-        '''
-        #ax.set_xscale('log')
-        #plt.xlim((1, 1000000))
-        #plt.legend()
-        pylab.savefig(
-          '%s/%s-%s-%s.pdf' % (
-            plot_dir,
-            re.sub('/', '_', psets_pretty[pset_idx]) if pset_idx < len(psets_pretty) else 'all',
-            material,
-            sub_type_definition[submission_type]
-          ),
-          format='pdf'
-        )
-        #plt.close()
+          for group_idx, group in enumerate(groups):
+            for username, sum_time in logs['%s_%s' % (group, material)].iteritems():
+              time.append(sum_time)
+              scores.append(float(user_lists[group][username][0]))
+          #ax.scatter(time, scores, c=colors[group_idx], label=group)
 
-  '''
-  for
+          pylab.xscale('log')
+          pylab.xlim((1, 1000000))
 
-  quantiztion_bins = None
-  if (sys.argv) > 4:
-    quantiztion_bins = int(sys.argv[4])
-
-  for l_id in open(mapping):
-    l_id = l_id.strip()
-    with open('%s/%s' % (word2vec_fea_dir, l_id)) as f:
-      try:
-        cos_sim_matrix = [[quantiztion(y, quantiztion_bins) for y in x.strip().split('\t')] for x in f.readlines()]
-      except ValueError:
-        print '%s/%s' % (word2vec_fea_dir, l_id)
-
-      fig = plt.figure()
-      plt.clf()
-      ax = fig.add_subplot(111)
-      res = ax.imshow(np.array(cos_sim_matrix), cmap=plt.cm.Greys_r, interpolation='nearest')
-      #res = ax.imshow(np.array(cos_sim_matrix), cmap=plt.cm.jet, interpolation='nearest')
-      ax.set_aspect('auto')
-
-      cb = fig.colorbar(res)
-      alphabet = '123456789'
-      width = len(cos_sim_matrix[0])
-      plt.xticks(range(width), alphabet[:width])
-      plt.savefig('%s/%s.pdf' % (word2vec_plot_dir, l_id), format='pdf')
-      plt.close()
-  '''
+          pylab.title('materials = %s, pset = %s, time span = %s, stratification = %s' % (
+            re.sub('_time', '', material),
+            (re.sub('/', '_', psets_pretty[pset_idx]) if pset_idx < len(psets_pretty) else 'all'),
+            submission_type,
+            user_cohort_name
+          ))
+          pylab.xlabel("spent time (seconds)")
+          pylab.ylabel("scores")
+          errorplot(time, scores, bins=[1,10,100,1000,10000,100000,1000000])
+          '''
+          # plot by groups
+          for group_idx, group in enumerate(groups):
+            (time, scores) = ([], [])
+            for username, sum_time in logs['%s_%s' % (group, material)].iteritems():
+              time.append(sum_time)
+              scores.append(float(user_lists[group][username][0]))
+            ax.scatter(time, scores, c=colors[group_idx], label=group)
+          ax.set_xscale('log')
+          plt.xlim((1, 1000000))
+          plt.legend()
+          '''
+          pylab.savefig(
+            '%s/%s/%s-%s-%s.pdf' % (
+              plot_dir,
+              user_cohort_name,
+              re.sub('/', '_', psets_pretty[pset_idx]) if pset_idx < len(psets_pretty) else 'all',
+              material,
+              submission_type
+            ),
+            format='pdf'
+          )
+          #plt.close()
 
 if __name__ == '__main__':
   _main( )
